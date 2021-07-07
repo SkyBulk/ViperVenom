a102bee2ef7c06d1830eccf2ea94bd5e922b949f
Itay Funes <45236616+0x4Hydro@users.noreply.github.com>
2021-07-07 20:32:03 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+
+# UNFINISHED STUFF, all it does is it will convert your client.py to an executable, but do not try it just yet or you'll run into problems...
+
 from distutils.core import setup
 import os
 import shutil
