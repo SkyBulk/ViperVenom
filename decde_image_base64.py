53f2d72dc01205e7f867bc9a80218440818051d5
Itay Funes <45236616+0x4Hydro@users.noreply.github.com>
2021-07-07 20:29:33 +0300

    Update decde_image_base64.py

--- decde_image_base64.py
+++ decde_image_base64.py
@@ -1,10 +1,10 @@
+# This is probably the easiest Python script I've ever made, really, all it does is to decrypt your base64 string on base64.txt and write the decrypted string to a photo, magic!
+
 import base64
-
-
 f=open('base64.txt', 'r')
 string=f.read()
 f.close()
 image=base64.b64decode(string)
 f=open('screenshot.png', 'wb')
-f.write(image)
+f.write(image) # here the magic happens 
 f.close()
