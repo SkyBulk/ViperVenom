65e85dc7ab769ec2a06b9cb637794faff9e17a39
Itay Funes <45236616+0x4Hydro@users.noreply.github.com>
2021-08-07 12:50:35 +0300

    Delete decode_image.py

--- decode_image.py
+++
@@ -1,9 +0,0 @@
-# Use this small tool when you need to decrypt a base64 image and write it to an image.
-import base64
-f=open('base64.txt', 'r')
-string=f.read()
-f.close()
-image=base64.b64decode(string)
-f=open('screenshot.jpg', 'wb')
-f.write(image) # here the magic happens 
-f.close()
